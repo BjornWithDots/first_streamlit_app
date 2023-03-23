4ed2015e2ad8eda1ed0c042106845d94c6710004
BjornWithDots <93321900+BjornWithDots@users.noreply.github.com>
2023-03-23 21:28:54 +0100

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner!')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Oatmeal')
+streamlit.text('Bacon and eggs')
+streamlit.text('Saucage and Beans')
