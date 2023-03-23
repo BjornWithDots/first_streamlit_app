4b8790c55ac4ea3047e8b9d5d1048d8d26c48d4d
BjornWithDots <93321900+BjornWithDots@users.noreply.github.com>
2023-03-23 21:39:27 +0100

    Update streamlit_app.py
    added next menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner!')
 
-  
-
 streamlit.header(' Breakfast menu')
 streamlit.text('🥣 Omega 3 and Oatmeal')
 streamlit.text('🥗 Kale, Spinache and Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
